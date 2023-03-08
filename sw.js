60e1c3f4c89e2ac02e60b88b98e4d8d974196a53
zik6789 <124240037+zik6789@users.noreply.github.com>
2023-03-08 18:23:21 +0700

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,8 @@
+self.options = {
+    "domain": "forlumineoner.com",
+    "resubscribeOnInstall": true,
+    "zoneId": 1767458,
+    "pub": 1
+}
+self.lary = "";
+importScripts('https://forlumineoner.com/pfe/current/service-worker.min.js?r=sw')
